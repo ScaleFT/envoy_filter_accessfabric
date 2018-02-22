871cdee8a56506a22d40312a92b874126c281ce6
Ben Coverston <ben.coverston@gmail.com>
2018-02-22 15:39:56 -0700

    add refinements to the shell script, and include README.md

--- build-tools/Docker/build-filter/build-envoy-plugin.sh
+++ build-tools/Docker/build-filter/build-envoy-plugin.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# example invocation with a non-master branch:
+# The first argument should be the github org, the second argument should be the branch name
+# sh build-envoy-plugin.sh bcoverston make-envoy-plugin-docker
+
+org=${1:-"ScaleFT"}
+branch=${2:-"master"}
+
 #TODO add build-arg support into this script. This will default to the master branch
-docker build -t envoybuild .
+docker build --build-arg org=$org --build-arg branch=$branch  -t envoybuild .
 
 docker run --name=evb -di envoybuild bash
 
